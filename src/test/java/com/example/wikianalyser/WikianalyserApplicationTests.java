42f7b30b9f8c83e4fb447d4cff1ba87bef151e8b
lamecode <kislaya.sliva@gmail.com>
2022-01-02 12:07:43 +0200

    props

---
+++ src/test/java/com/example/wikianalyser/WikianalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wikianalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikianalyserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
