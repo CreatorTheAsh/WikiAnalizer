eab76334a4ef60df4d8288f611f5f929f82bc74f
lamecode <kislaya.sliva@gmail.com>
2022-01-04 01:07:15 +0200

    Update XMLParser.java

--- src/main/java/com/example/wikianalyser/model/XMLParser.java
+++ src/main/java/com/example/wikianalyser/model/XMLParser.java
@@ -1,5 +1,25 @@
 package com.example.wikianalyser.model;
 
-public class XMLParser {
+import org.apache.commons.lang3.StringUtils;
 
+public class XMLParser {
+    public static String parse(String xml) {
+        String[] items = xml.split("<item>");
+        String item;
+        String title;
+        String pubDate;
+        String author;
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 1; i < items.length; i++) {
+            item = items[i];
+            title = StringUtils.substringBetween(item, "<title>", "</title>");
+            builder.append(title + " | ");
+            pubDate = StringUtils.substringBetween(item, "<pubDate>", "</pubDate>");
+            builder.append(pubDate + " | ");
+            author = StringUtils.substringBetween(item, "<dc:creator>", "</dc:creator>");
+            builder.append(author + "\n");
+        }
+        return builder.toString();
+    }
 }
