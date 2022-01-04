34099460145b4775fb4b71358f86112113013daf
lamecode <kislaya.sliva@gmail.com>
2022-01-04 11:45:58 +0200

    reactive stream fetches data!

--- src/main/java/com/example/wikianalyser/controller/DataController.java
+++ src/main/java/com/example/wikianalyser/controller/DataController.java
@@ -2,7 +2,9 @@ package com.example.wikianalyser.controller;
 
 import com.example.wikianalyser.model.JSONParser;
 import com.example.wikianalyser.model.XMLParser;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,24 +14,31 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+import java.time.LocalTime;
 import java.util.concurrent.Flow;
 
 @Controller
 public class DataController {
 
-    @GetMapping(path = "/recentChangesstreaming", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(path = "/recentChangesstreaming")
     @ResponseBody
-    public Mono<String> getRecentChanges() {
-        Mono<String> stringFlux =  WebClient.create()
-                .get().uri("https://uk.wikipedia.org/w/api.php?" +
-                        "action=feedrecentchanges")
-                .retrieve().bodyToFlux(String.class)
-                .take(100)
-                .delaySubscription(Duration.ofSeconds(3))
-                .repeat()
-                .reduce("", (s1, s2) -> s1 + s2)
-                .map(s -> XMLParser.parse(s));
-        return stringFlux;
+    public Flux<ServerSentEvent<String>> getRecentChanges() {
+        WebClient client = WebClient.create("https://stream.wikimedia.org/v2");
+        ParameterizedTypeReference<ServerSentEvent<String>> type
+                = new ParameterizedTypeReference<>() {};
+
+        Flux<ServerSentEvent<String>> eventStream = client.get()
+                .uri("/stream/recentchange")
+                .retrieve()
+                .bodyToFlux(type)
+                .delaySubscription(Duration.ofSeconds(5))
+                .take(2)
+                .repeat();
+        eventStream.subscribe(
+                content -> System.out.println("gregreg"),
+                error -> System.out.println("error"),
+                () -> System.out.println("complete"));
+        return eventStream;
     }
 
     @GetMapping(path = "/recentActivity&username={username}",
@@ -40,7 +49,7 @@ public class DataController {
                 .get().uri("https://uk.wikipedia.org/w/api.php?" +
                         "action=query&format=json&list=usercontribs&ucuser=" + username)
                 .retrieve().bodyToFlux(String.class)
-                .delaySubscription(Duration.ofSeconds(3))
+                .delaySubscription(Duration.ofSeconds(5))
                 .reduce("", (s1, s2) -> s1 + s2)
                 .map(s -> JSONParser.parse(s));
                 //.repeat();
@@ -54,7 +63,7 @@ public class DataController {
                 .get().uri("https://uk.wikipedia.org/w/api.php?" +
                         "action=query&format=json&list=usercontribs&ucuser=" + username)
                 .retrieve().bodyToFlux(String.class)
-                .delaySubscription(Duration.ofSeconds(3))
+                .delaySubscription(Duration.ofSeconds(5))
                 .reduce("", (s1, s2) -> s1 + s2)
                 .map(s -> JSONParser.getStats(s));
         //.repeat();
